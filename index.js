f0b776662ecee7d3e1184f55e533495a291b3611
MatLemes16 <matheus_lemes_01@live.com>
2024-04-14 21:32:39 -0300

    Finalizado

---
+++ index.js
@@ -0,0 +1,18 @@
+let qualMochila = "Mochila da hummel";
+let itnesMochila = "Tenis, Meia, Calça, Coquilha, Uniforme";
+let utenciliosCorretos;
+
+if (itnesMochila === "Calça, Meia, Tenis, Uniforme, Coquilha" ) {
+    utenciliosCorretos = "desorganizada"}
+
+else if (itnesMochila === "Tenis, Meia, Calça, Coquilha, Uniforme" ){
+    utenciliosCorretos = "tudo que eu preciso"
+
+}
+
+else {utenciliosCorretos === "Tenis social, meia soquete, calça jeans, camisa social" };
+
+
+
+
+console.log("A " + qualMochila + " contém " + utenciliosCorretos );
